627cca7cf20ca016f3d4efe10aff7b6852d5aa48
ZQA <zkvpz@naver.com>
2018-07-14 21:48:32 +0900

    유니티 프로젝트 생성

---
+++ aTestUnity_2018_1_6_f1/Assets/Test/TestScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// 테스트를 위한 스크립트.
+
+public class TestScript : MonoBehaviour
+{
+
+	private void Awake()
+	{
+
+	}
+
+	private void Start()
+	{
+
+	}
+
+	private void Update()
+	{
+
+	}
+
+}
